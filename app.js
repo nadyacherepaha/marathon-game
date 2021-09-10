a38d80109fa531689c068b5619cbbf132d0f7c2f
nadyacherepaha <nad.cherepaha@gmail.com>
2021-09-10 20:18:27 +0300

    Add transitions between screens

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
 const startBtn = document.querySelector('#start')
 const screens = document.querySelectorAll('.screen')
+const timeList = document.querySelector('#time-list')
+const timeEl = document.querySelector('#time')
+let time = 20
 
 startBtn.addEventListener('click', (event) => {
   event.preventDefault()
   screens[0].classList.add('up')
 })
+
+timeList.addEventListener('click', event => {
+  if (event.target.classList.contains('time-btn')) {
+    time = parseInt(event.target.getAttribute('data-time'))
+    screens[1].classList.add('up')
+    startGame()
+  }
+})
+
+startGame()
+
+function startGame() {
+  setInterval(decreaseTime, 1000)
+  setTime(time)
+}
+
+function decreaseTime() {
+  if (time === 0) {
+    finishGame()
+  } else {
+    let current = --time
+    if (current < 10) {
+      current = `0${current}`
+    }
+    setTime(current)
+  }
+}
+
+function setTime(value) {
+  timeEl.innerHTML = `00:${value}`
+}
+
+function finishGame() {
+  
+}
